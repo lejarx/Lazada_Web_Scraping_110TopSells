1fdc650c26f5022fcbcf9aedb86e20d031034091
che.joharia <chejoharia@eisti.fr>
2015-12-16 08:30:41 +0800

    Add web scraping procedures
    Iterate through 5 different Lazada pages and for each page, get all the top selling items.
    Write to output all these items.
    Save file with the today's date.

--- WebScrapLazadaTop110Sells.py
+++ WebScrapLazadaTop110Sells.py
@@ -46,3 +46,52 @@ f.write('day-ranking'
 	+'\t'+'price-normal'
 	+'\t'+'price-discount'
 	+'\t'+'discount'+'\n')
+
+   
+############################################################
+#
+# Scrap data from Lazada web pages
+# - iterate 4 different pages to get all top 110 selling products
+#
+############################################################
+
+for x in range(1,5):
+	url = 'http://www.lazada.com.my/highlights-top-sellers/?page='+str(x)	
+	page = urlopen(url)
+	bs_page = BeautifulSoup(page,'lxml')
+
+	catalog_content = bs_page.find('div',class_='catalog-content-wrapper')
+	products = catalog_content.find_all('div', class_='product-description')
+
+	# Write products to file
+	for product in products:
+		# check for price-normal
+		if product.find("span",class_='product-price-normal').string is None:
+			price_normal = ''
+		else:
+			price_normal = product.find("span",class_='product-price-normal').string
+		# check for price-discount
+		if product.find("span",class_='product-price-discount') is None:
+			price_discount = ''
+		else:
+			price_discount = product.find("span",class_='product-price-discount').string
+		# check for product-discount
+		if product.find("div",class_='product-discount') is None:
+			product_discount = ''
+		else:
+			product_discount = product.find("div",class_='product-discount').string
+
+		f.write(product.a['data-position']
+		+'\t'+product.a['data-sku']
+		+'\t'+product.img['alt']
+		+'\t'+price_normal
+		+'\t'+price_discount
+		+'\t'+product_discount+'\n')
+
+############################################################
+#
+# Close file
+#
+############################################################
+
+f.close()
