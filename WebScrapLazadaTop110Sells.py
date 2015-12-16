629a498b0b854e219c103332628849d8b4e4dbee
che.joharia <chejoharia@eisti.fr>
2015-12-16 08:25:59 +0800

    Create WebScrapLazadaTop110Sells.py
    file init

---
+++ WebScrapLazadaTop110Sells.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 08:05:42 2015
+@python_version Python3
+
+@context: MaGIC Analytics 
+@prof: goh.yongkheng_at_gmail.com
+@author: chejoharia
+
+@purpose: WebScraping Excercise
+@         - Extract Lazada Daily Top 110 top sellers
+@input      : http://www.lazada.com.my/highlights-top-sellers/?page=1'	
+@output     : a tab delimted dataset in txt file of today's 110 top selling items
+"""
+
